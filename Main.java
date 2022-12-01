52c3c343854c044c3155a561b0cb8261b5653d85
KadirCengiz <115431038+KadirCengiz@users.noreply.github.com>
2022-12-01 13:31:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        char vChar='b';
+        char v2Char=98;
+        System.out.println(vChar);
+        System.out.println(v2Char);
+
+        char c1='J';
+        char c2='A';
+        char c3='V';
+        char c4='A';
+        System.out.println(c1+c2+c3+c4);
+        System.out.println(""+c1+c2+c3+c4);
+
+        char letter='u';
+        System.out.println(letter);
+
+        boolean dogru=true;
+        boolean yanlis=false;
+        System.out.println(dogru);
+        System.out.println(yanlis);
+
+        boolean logic1=true;
+        boolean logic2=false;
+        System.out.println(logic1);
+        System.out.println(logic2);
+
+
+        String vStr="Java101 Patikası";
+        String words="Java'ya Hoşgeldiniz";
+        System.out.println(vStr);
+        System.out.println(words);
+    }
+}
